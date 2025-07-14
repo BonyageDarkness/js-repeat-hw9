21af5c4e7df884eab52c646a4be943ff16af5f72
Bohdan <bosrok.ke@gmail.com>
2025-07-14 21:08:05 +0100

    hw9-1

---
+++ src/js/2-form.js
@@ -0,0 +1,8 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
--- src/main.js
+++
@@ -1,3 +0,0 @@
-console.log('first');
-const value = 10;
-console.log(value);
